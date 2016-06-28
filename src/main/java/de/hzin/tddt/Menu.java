504d399d63f2471ea4e22ace92d21e5dd786cc50
RafaelPin <Rafael.Pinela.Da.Silva@live.de>
2016-06-28 18:24:55 +0200

    updated time

--- src/main/java/de/hzin/tddt/Menu.java
+++ src/main/java/de/hzin/tddt/Menu.java
@@ -3,15 +3,10 @@ package de.hzin.tddt;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-/**
- * Created by Rafael on 27.06.2016.
- */
 public class Menu {
 
     @FXML
     public Label timecounter;
-
-
 
 
 }
