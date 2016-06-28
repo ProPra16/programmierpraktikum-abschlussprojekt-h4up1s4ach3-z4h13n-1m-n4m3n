b98e3ce8a639aefdd2cc593c8b8eaebce16cf074
jkliss <julius.kliss@gmail.com>
2016-06-28 17:35:42 +0200

    revert

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -12,6 +12,10 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        String[] names = new String[2];
+        names[0] = "LeapYear";
+        names[1] = "LeapYearTest";
+        Compilation test = new Compilation(names);
         Group root = new Group();
         primaryStage.setScene(new Scene(root, 100, 100));
         primaryStage.show();
