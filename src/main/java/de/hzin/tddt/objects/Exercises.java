451c65244486184464dddc8a687aa9d2249a45c0
Velomaniac <a.weisermann@gmail.com>
2016-07-07 18:46:37 +0200

    added currentExercise and getExerciseByIndex

--- src/main/java/de/hzin/tddt/objects/Exercises.java
+++ src/main/java/de/hzin/tddt/objects/Exercises.java
@@ -13,12 +13,24 @@ import java.util.List;
  */
 @XmlRootElement(name = "exercises")
 public class Exercises {
-
     private List<Exercise> exercises = new ArrayList<>();
+    private int currentExercise = 0;
 
     @XmlElement(name = "exercise")
     public List<Exercise> getExercisesList() {
         return exercises;
+    }
+
+    public Exercise getExerciseByIndex(int index){
+        return exercises.get(index);
+    }
+
+    public void setCurrentExercise(int i){
+        currentExercise = i;
+    }
+
+    public int getCurrentExercise(){
+        return currentExercise;
     }
 
     public void setExercises(List<Exercise> exercises) {
