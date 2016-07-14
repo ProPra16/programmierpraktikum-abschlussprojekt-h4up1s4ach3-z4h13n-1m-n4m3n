18fe5825df1d4c2107618f3bd53212c1559a74aa
Velomaniac <a.weisermann@gmail.com>
2016-07-14 15:06:11 +0200

    change ExerciseView to TreeView

--- src/main/java/de/hzin/tddt/panes/ExerciseView.java
+++ src/main/java/de/hzin/tddt/panes/ExerciseView.java
@@ -1,59 +1,74 @@
 package de.hzin.tddt.panes;
 
+import de.hzin.tddt.MainWindowController;
 import de.hzin.tddt.objects.Exercise;
 import de.hzin.tddt.objects.ExerciseClass;
 import de.hzin.tddt.objects.Exercises;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Aron Weisermann
  */
-public class ExerciseView extends TreeView<String> {
-    public ExerciseView(Exercises exercises) {
-        List<Exercise> exerciseList = exercises.getExercisesList();
-        TreeItem<String> rootNode =
-                new TreeItem<String>(exercises.getFile().getName());
-        rootNode.setExpanded(true);
+public class ExerciseView extends TreeView {
 
-        for (Exercise exercise : exerciseList) {
-            TreeItem<String> exNode = new TreeItem<String>(exercise.getName());
+    public ExerciseView(MainWindowController controller) {
+        Exercises exercises = controller.exercises;
+        List<Exercise> exerciseList = exercises.getExercisesList();
+        TreeItem rootNode = new TreeItem<>(exercises.getFile().getName());
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        for (int i = 0; i < exerciseList.size(); i++) {
+            Exercise exercise = exerciseList.get(i);
+            RadioButton exRadioBtn = new RadioButton(exercise.getName());
+            exRadioBtn.setToggleGroup(toggleGroup);
+
+            TreeItem<RadioButton> exNode = new TreeItem<>(exRadioBtn);
             for (ExerciseClass exClass : exercise.getClasses()) {
-                TreeItem<String> clNode = new TreeItem<String>(exClass.getName());
-                TreeItem<String> teNode = new TreeItem<String>(exClass.getTest().getName());
+                TreeItem clNode = new TreeItem<>(exClass.getName());
+                TreeItem teNode = new TreeItem<>(exClass.getTest().getName());
+
                 exNode.getChildren().addAll(clNode, teNode);
             }
+
+            exRadioBtn.addEventFilter(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> {
+                if (isChangeExerciseConfirmed()) {
+                    exRadioBtn.setSelected(true);
+                    event.consume();
+                }
+            });
+
             rootNode.getChildren().add(exNode);
         }
+        toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (toggleGroup.getSelectedToggle() != null) {
+                int selectedIndex = toggleGroup.getToggles().indexOf(toggleGroup.getSelectedToggle());
+                controller.saveCurrentFile();
+                exercises.setCurrentIndex(selectedIndex);
+                controller.replaceCodeAreaTextToCurrent();
+            }
+        });
+
+        rootNode.setExpanded(true);
         setRoot(rootNode);
     }
 
-    /*public ExerciseView(Exercises exercises, String exercisesName, CodeArea codeArea) {
-        List<Exercise> exerciseList = exercises.getExercisesList();
-        setText("Aktuelles " + exercisesName);
-        exercisePanes = new TitledPane[exerciseList.size()];
-        for (int i = 0; i < exerciseList.size(); i++) {
-            ListView<String> classesListView = new ListView();
-            for (ExerciseClass cur : exerciseList.get(i).getClasses()) {
-                classesListView.getItems().add(cur.getName());
-                classesListView.getItems().add(cur.getTest().getName());
-            }
-            int curI = i;
-            classesListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                public void handle(MouseEvent me) {
-                    exercises.setCurrentIndex(curI);
-                    if(classesListView.getSelectionModel().getSelectedItem().contains("Test"))
-                        codeArea.replaceText(exercises.getCurrentExercise().getClasses().get(0).getTest().getCode());
-                    else
-                        codeArea.replaceText(exercises.getCurrentExercise().getClasses().get(0).getCode());
-                }
-            });
-            exercisePanes[i] = new TitledPane("Übung: " + exerciseList.get(i).getName(), classesListView);
+    public boolean isChangeExerciseConfirmed() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Sind Sie sicher, dass Sie die Übung wechseln möchten?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        System.out.println(result.get());
+        if (result.get().equals(ButtonType.OK)) {
+            System.out.println(true);
+            return true;
+        } else {
+            System.out.println(false);
+            return false;
         }
-        VBox group = new VBox();
-        group.getChildren().addAll(exercisePanes);
-        setContent(group);
-    }*/
+    }
 }
